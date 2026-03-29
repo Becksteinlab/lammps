16e33938d768b1ce826aa2b7d31db58bb6bf4f16
Axel Kohlmeyer <akohlmey@gmail.com>
2026-03-28 22:06:19 -0400

    write nterms_max to restart and read it back

--- src/EXTRA-MOLECULE/dihedral_nharmonic.cpp
+++ src/EXTRA-MOLECULE/dihedral_nharmonic.cpp
@@ -291,7 +291,11 @@ void DihedralNHarmonic::coeff(int narg, char **arg)
 
 void DihedralNHarmonic::write_restart(FILE *fp)
 {
+  // must store nterms_max in restart file in addition to the nterms array
+  // the KOKKOS version requires it to store the coefficients in a 2d view
+  fwrite(&nterms_max,sizeof(int),1,fp);
   fwrite(&nterms[1],sizeof(int),atom->ndihedraltypes,fp);
+
   for (int i = 1; i <= atom->ndihedraltypes; i++)
     fwrite(a[i],sizeof(double),nterms[i],fp);
 }
@@ -304,9 +308,11 @@ void DihedralNHarmonic::read_restart(FILE *fp)
 {
   allocate();
 
-  if (comm->me == 0)
+  if (comm->me == 0) {
+    utils::sfread(FLERR,&nterms_max,sizeof(int),1,fp,nullptr,error);
     utils::sfread(FLERR,&nterms[1],sizeof(int),atom->ndihedraltypes,fp,nullptr,error);
-
+  }
+  MPI_Bcast(&nterms_max,1,MPI_INT,0,world);
   MPI_Bcast(&nterms[1],atom->ndihedraltypes,MPI_INT,0,world);
 
   // allocate
