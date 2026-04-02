15b5a7a5318b2cec7aa124a77977a84b3a515772
Amruthesh Thirumalaiswamy <amruthesht@users.noreply.github.com>
2026-04-02 15:08:28 -0700

    update to omp script

--- bin/build-run-scripts/build_openmp.sh
+++ bin/build-run-scripts/build_openmp.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 module load openmpi/4.1.2
 
+VERSION="imdv3"
+
 PROJECT_ROOT=$(pwd)/../..
 BUILD_DIR="$PROJECT_ROOT/bin/build-openmp"
 RUN_DIR="$PROJECT_ROOT/bin/run-openmp"
-EXEC_NAME="lmp_openmp"
 
+IMD_ASYNC="ON"            # ON or OFF
+
+rm -rf "$BUILD_DIR"
 mkdir -p "$BUILD_DIR" "$RUN_DIR"
 
+EXEC_NAME="lmp_openmp_${VERSION}_${IMD_ASYNC}"
+
+cd "$PROJECT_ROOT"
+git checkout "${VERSION}-benchmarking"
+
+# LAMMPS with MPI + OpenMP only (no CUDA/Kokkos/GPU)
 cmake -S "$PROJECT_ROOT/cmake" -B "$BUILD_DIR" \
   -D CMAKE_BUILD_TYPE=Release \
   -D CMAKE_CXX_FLAGS_RELEASE="-march=native" \
@@ -17,13 +27,20 @@ cmake -S "$PROJECT_ROOT/cmake" -B "$BUILD_DIR" \
   -D PKG_GPU=OFF \
   -D PKG_CHARMM=ON \
   -D PKG_CMAP=ON \
-  -D PKG_KSPACE=ON \
-  -D PKG_MOLECULE=ON \
-  -D PKG_RIGID=ON \
-  -D PKG_OPENMP=ON \
+  -D PKG_COLVARS=ON \
   -D PKG_SHAKE=ON \
+  -D PKG_KSPACE=ON \
+  -D PKG_RIGID=ON \
+  -D PKG_MOLECULE=ON \
+  -D PKG_MISC=ON \
+  -D LAMMPS_ASYNC_IMD=${IMD_ASYNC} \
+  -D PKG_OPENMP=ON \
+  -D PKG_EXTRA-FIX=ON \
+  -D PKG_EXTRA-DUMP=ON \
+  -D PKG_MOLFILE=ON \
   -D LAMMPS_MACHINE=openmp
 
-cmake --build "$BUILD_DIR" -- -j4
+cmake --build "$BUILD_DIR" -- -j16
 
+# copy executable to run dir
 cp "$BUILD_DIR/lmp_openmp" "$RUN_DIR/$EXEC_NAME"
